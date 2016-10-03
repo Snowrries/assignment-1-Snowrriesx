6ccdbcb0d01f6ff3eb09bd541f150a8648addd0e
Snow Rries <Snow Rries>
2016-10-03 15:15:20 -0400

    Initial commit

---
+++ app/src/main/java/crumbled/kewhykrumm/assignment_1_snowrries/ToDo.java
@@ -0,0 +1,13 @@
+package crumbled.kewhykrumm.assignment_1_snowrries;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToDo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do);
+    }
+}
